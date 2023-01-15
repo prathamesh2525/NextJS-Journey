96777d4587f117f5fb4bc00adf4c6a3436aa9bdf
prathamesh2525 <prathameshmadniwale31@gmail.com>
2023-01-15 13:52:39 +0530

    static site generation - explored

---
+++ pages/static/users/[id].jsx
@@ -0,0 +1,33 @@
+import React from "react"
+
+const UserPage = (props) => {
+  return (
+    <div>
+      <h1>Profile page of {props.data.firstName}</h1>
+    </div>
+  )
+}
+
+export const getStaticPaths = async () => {
+  const data = await (await fetch(`https://dummyjson.com/users/`)).json()
+  const allUsersIds = data.users.map((user) => user.id)
+
+  console.log("All USers Ids", allUsersIds)
+ 
+  return {
+    paths: allUsersIds.map((userId) => ({ params: { id: `${userId}` } })),
+    fallback: false,
+  }
+}
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id
+  const data = await (await fetch(`https://dummyjson.com/users/${id}`)).json()
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+export default UserPage
